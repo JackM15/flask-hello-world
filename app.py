c441f0f7095bfccacc036b44e58c3baa9eb2943f
Jack M <jmartin@xmostrata.com>
2017-02-23 13:12:08 +0000

    Added Dynamic Routes

--- app.py
+++ app.py
@@ -6,12 +6,45 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+# error handling
+app.config["DEBUG"] = True
+
 # use the decorator pattern to link the view function to a url
 @app.route("/")
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
     return "Hello, World!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+#int type in route
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+#float type in route
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+#dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Hello, {}, you're not michael though!".format(name), 404
 
 #start the development server using the run() method
 if __name__ == "__main__":
